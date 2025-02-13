7b40c473be1533c4004ed31593b6afe1370f0e7a
Igwe Chinonso <igwechinonso77@gmail.com>
2025-02-13 20:57:20 +0100

    Fix like button

--- queries/usePost.ts
+++ queries/usePost.ts
@@ -1,4 +1,3 @@
-
 import { useQuery } from "@tanstack/react-query";
 import { baseUrl } from "@/utils/constant";
 import Cookies from "js-cookie";
@@ -7,19 +6,19 @@ export const useFetchPosts = () => {
   return useQuery({
     queryKey: ["posts"],
     queryFn: async () => {
-      const token = Cookies.get("accessToken")
+      const token = Cookies.get("accessToken");
       const header = {
-        Authorization: `${token ? `Bearer ${token}` : ""}`
-      }
+        Authorization: `${token ? `Bearer ${token}` : ""}`,
+      };
       const response = await fetch(
-        // `${baseUrl}/posts?page=1&limit=10`,
-        `${baseUrl}/users/10804/posts?page=1&limit=10`,
+        `${baseUrl}/posts?page=1&limit=10`,
+        // `${baseUrl}/users/10804/posts?page=1&limit=10`,
         {
-          headers: { ...header }
+          headers: { ...header },
         }
       );
       const result = await response.json();
-      return result
-    }
+      return result;
+    },
   });
-}
+};
