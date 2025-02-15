ecdbf64ee710e30359d5f193c791eadc9f82a637
Igwe Chinonso <igwechinonso77@gmail.com>
2025-02-15 12:14:33 +0100

    make comment section responsive

--- components/Socials/Comments.tsx
+++ components/Socials/Comments.tsx
@@ -3,15 +3,15 @@ import CommentSectionSkeleton from "../sketetons/CommentSectionSkeleton";
 import CommentItem from "./CommentItem";
 import Input from "@/components/ui/Input";
 import Link from "next/link";
-import Image from "next/image";
 import { useFetchComments } from "@/hooks/useFetchComments";
-import { X } from "lucide-react";
+import { Heart, X } from "lucide-react";
 import { Avatar, AvatarImage, AvatarFallback } from "../ui/avatar";
 import { useAuth } from "@/context/AuthContext";
 import { useMutation } from "@tanstack/react-query";
 import { useState } from "react";
 import { addCommentService } from "@/services/comment.service";
-import { CommentPayload } from "@/types/index";
+import Image from "next/image";
+import { CommentPayload } from "@/types";
 
 export function Comments({ post, onClose }: { post: any; onClose?: () => void }) {
   const { data: comments, isPending: isFetchingComments } = useFetchComments(post?.id);
@@ -35,7 +35,7 @@ export function Comments({ post, onClose }: { post: any; onClose?: () => void })
   };
 
   return (
-    <div className="relative flex gap-4 flex-col md:flex-row h-full w-full">
+    <div className="relative flex flex-col gap-2 md:flex-row h-full w-full">
       {/* Media Section */}
       <div className="bg-black/90 rounded-xl md:basis-5/12 w-full md:w-auto">
         <MediaWrapper
@@ -49,8 +49,8 @@ export function Comments({ post, onClose }: { post: any; onClose?: () => void })
 
       {/* Drawer (Tablet & Mobile) */}
       <div
-        className={`fixed md:relative bottom-0 rounded-xl left-0 w-full md:flex-1 h-[80vh] bg-white border rounded-t-xl md:rounded-xl transition-transform duration-300 ${isDrawerOpen ? "translate-y-0" : "translate-y-[90%] md:translate-y-0"
-          } md:h-auto flex flex-col`}
+        className={`fixed md:relative bottom-0 left-0 roundex-xl w-full md:flex-1 h-[80vh] bg-white border rounded-t-xl md:rounded-xl transition-transform duration-300 ${isDrawerOpen ? "translate-y-0" : "translate-y-[90%] md:translate-y-0"
+          } md:h-auto flex flex-col overflow-hidden`}
       >
         <div className="flex justify-between items-center p-3 md:hidden">
           <p className="font-semibold">Comments</p>
@@ -65,7 +65,7 @@ export function Comments({ post, onClose }: { post: any; onClose?: () => void })
             <p>Be the first to comment</p>
           </div>
         ) : (
-          <div className="flex-1 overflow-y-auto p-2">
+          <div className="flex-1 overflow-y-auto p-2 max-h-[60vh] md:max-h-[73vh]">
             {comments?.data?.comments?.map((comment: any) => (
               <CommentItem key={comment.id} comment={comment} />
             ))}
@@ -75,11 +75,13 @@ export function Comments({ post, onClose }: { post: any; onClose?: () => void })
         {/* Input Section */}
         <div className="w-full p-2 border-t mt-auto">
           {currentUser ? (
-            <div className="flex items-center gap-2 w-full">
-              <Avatar className="w-12 h-12 basis-1.5/12">
-                <AvatarImage src={currentUser?.avatar} alt="User avatar" />
-                <AvatarFallback>.</AvatarFallback>
-              </Avatar>
+            <div className="flex items-center justify-between gap-2 w-full">
+              <div className="basis-1.5/12">
+                <Avatar className="w-12 h-12">
+                  <AvatarImage src={currentUser?.avatar} alt="User avatar" />
+                  <AvatarFallback>.</AvatarFallback>
+                </Avatar>
+              </div>
               <div className="flex-1">
                 <Input
                   variant="comment"
@@ -105,7 +107,7 @@ export function Comments({ post, onClose }: { post: any; onClose?: () => void })
 
       {/* Floating Comments Button (Tablet & Mobile) */}
       <button
-        className="md:hidden fixed bottom-20 right-4 bg-primary-700 text-white h-10 w-10 rounded-full shadow-lg"
+        className="md:hidden fixed bottom-20 right-4 bg-primary-700 text-white w-10 h-10 rounded-full shadow-lg"
         onClick={() => setIsDrawerOpen(true)}
       >
         💬
