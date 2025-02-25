5888332e1e58ef3b2e989e0519e84e00658e7d37
Igwe Chinonso <igwechinonso77@gmail.com>
2025-02-25 08:04:53 +0100

    watchlist

---
+++ components/socials/watchlist/Playlist.tsx
@@ -0,0 +1,37 @@
+import { Progress } from '@/components/ui/progress'
+import { Thumbnail } from '@/public/assets'
+import { Clock } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import React from 'react'
+
+const Playlist = ({ value }: { value: number }) => {
+  return (
+    <div className='w-64 relative'>
+      <div className='absolute top-2 text-sm left-2 bg-red-600 text-white px-2 py-1 rounded-bl-md rounded-tr-md'>
+        Basic
+      </div>
+      <Image src={Thumbnail} alt="Thumbnail" />
+
+      <div>
+        <h2 className='text-sm'>Build it in Figma: Create a design system — Components</h2>
+        <div className='my-2'>
+          <Progress value={value} className={`${value > 50 ? "[&>*]:bg-green-600" : "[&>*]:bg-red-600"} h-1.5`} max={100} />
+        </div>
+        <p className='mb-2 text-sm text-gray-500'>{value}% complete</p>
+        <div className='flex items-center gap-2'>
+          <Clock size={14} />
+          <p>45:00mins</p>
+        </div>
+        <Link href='/socials/watchlist/playlist' className='text-sm hover:underline'>
+          View
+        </Link>
+      </div>
+      <div className='absolute bottom-[50%] text-sm right-2 bg-gray-700 text-white px-2 py-1 rounded-tr-md rounded-bl-md'>
+        15:00 mins
+      </div>
+    </div>
+  )
+}
+
+export default Playlist
