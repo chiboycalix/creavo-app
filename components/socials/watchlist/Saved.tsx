57b82da1d5170289019331b84fd4f0d0c29ce02b
Igwe Chinonso <igwechinonso77@gmail.com>
2025-02-25 08:05:40 +0100

    watchlist

--- components/socials/watchlist/Saved.tsx
+++ components/socials/watchlist/Saved.tsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import Playlist from './Playlist'
 
 const Saved = () => {
   return (
-    <div>Saved</div>
+    <div className='grid grid-cols-4 gap-4'>
+      <Playlist
+        value={34} />
+      <Playlist value={25} />
+      <Playlist value={18} />
+      <Playlist value={51} />
+      <Playlist
+        value={31} />
+      <Playlist value={85} />
+      <Playlist value={18} />
+      <Playlist value={51} />
+      <Playlist
+        value={64} />
+      <Playlist value={100} />
+      <Playlist value={18} />
+      <Playlist value={8} />
+    </div>
   )
 }
 
